84f803b8a5c28948524583f544217e43bf1f5b17
GogglesTv <jalenmarquise.moore@gmail.com>
2024-11-11 20:25:15 -0600

    Added a function that gets human to choose a weapon

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // List of weapons in an array
-const weapons = ["Rock", "Paper", "Scissors"];
+const weapons = ["rock", "paper", "scissors"];
 
 const getComputerChoice = function () {
   // generate random # 1-3
   let randomNumber = Math.floor(Math.random() * 3);
-  console.log(randomNumber);
 
   // use the number to choose a weapon
-  return weapons[randomNumber];
+  let computerWeapon = weapons[randomNumber];
+
+  return computerWeapon;
 };
 getComputerChoice();
+
+const getHumanChoice = function () {
+  let humanWeapon = prompt("Rock, Paper, or Scissors?");
+
+  console.log(humanWeapon);
+  return humanWeapon;
+};
+getHumanChoice();
